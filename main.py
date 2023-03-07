04128ac39f941b091e9bfc0c5823c12ca15fa074
Ashish <97246854+6abc@users.noreply.github.com>
2023-03-07 21:00:01 +0000

    added connection to db

--- main.py
+++ main.py
@@ -1,41 +1,19 @@
 from flask import Flask, render_template, jsonify
+from database import load_jobs_from_db
 
 # IN OUR EXAMPLE WE LABLED IT AS [main.py]
 main = Flask(__name__)
 
-JOBS = [{
-  'id': 1,
-  'title': 'Data Analyst',
-  'location': 'Bangaluru, India',
-  'salary': 'Rs. 10,00,000.00'
-}, {
-  'id': 2,
-  'title': 'Data Scientist',
-  'location': 'Delhi, India',
-  'salary': 'Rs. 15,00,000.00'
-}, {
-  'id': 3,
-  'title': 'Frontend Engineer',
-  'location': 'Remote',
-  'salary': 'Rs. 12,00,000.00'
-}, {
-  'id': 4,
-  'title': 'Backend Engineer',
-  'location': 'San Francisco, USA',
-  'salary': '$ 120,000.00'
-}]
-
-
 # IN OUR EXAMPLE WE LABLED IT AS [main.py]
 @main.route("/")
 def hello_world():
-  return render_template('home.html', jobs_list=JOBS, company_name='Ashish')
+  jobs = load_jobs_from_db()
+  return render_template('home.html', jobs=jobs, company_name='Ashish')
 
-
-@main.route("/api/jobs")
-def api_info():
-  return jsonify(JOBS)
-
+# @main.route("/api/jobs")
+# def api_info():
+# # jobs = load_jobs_from_db()
+#   return jsonify(load_jobs_from_db())
 
 #_______________________________________________________________________
 # Save it as [hello.py] or something similar. Make sure to not call your application flask.py because this would conflict with Flask itself.
